9c4855ea084cfbce11cabeafaa3a9f7450cd32c2
Gucio <kacper.gogol@gmail.com>
2026-01-23 14:52:38 +0100

    Add desc

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -1,18 +1,23 @@
 output "cluster_id" {
-  value = azurerm_kubernetes_cluster.this.id
+  description = "ID zasobu klastra AKS w Azure"
+  value       = azurerm_kubernetes_cluster.this.id
 }
 
 output "cluster_name" {
-  value = azurerm_kubernetes_cluster.this.name
+  description = "Nazwa utworzonego klastra AKS"
+  value       = azurerm_kubernetes_cluster.this.name
 }
 
 output "kube_config_raw" {
-  value     = azurerm_kubernetes_cluster.this.kube_config_raw
-  sensitive = true
+  description = "Surowa konfiguracja kubeconfig (do użycia z kubectl)"
+  value       = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive   = true
 }
 
 output "cluster_endpoint" {
-  value = azurerm_kubernetes_cluster.this.kube_config.0.host
+  description = "Adres endpointu API serwera Kubernetes"
+  # POPRAWKA: Zmiana z .0.host na [0].host
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].host
 }
 
 output "identity_principal_id" {
