378def392397a1a1a24b5177ea70debaec713228
Gucio <kacper.gogol@gmail.com>
2026-01-23 14:56:36 +0100

    fmt omg

--- modules/aks/outputs.tf
+++ modules/aks/outputs.tf
@@ -16,7 +16,7 @@ output "kube_config_raw" {
 
 output "cluster_endpoint" {
   description = "Adres endpointu API serwera Kubernetes"
-  value = azurerm_kubernetes_cluster.this.kube_config[0].host
+  value       = azurerm_kubernetes_cluster.this.kube_config[0].host
 }
 
 output "identity_principal_id" {
