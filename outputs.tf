40b14a8a49f866a875997c41ad1b3041dea46567
Gucio <kacper.gogol@gmail.com>
2026-02-02 16:55:01 +0100

    fix: Exchange vars desc and outputs to eng

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "cluster_id" {
-  description = "ID zasobu klastra AKS w Azure"
+  description = "The Azure resource ID of the AKS cluster"
   value       = azurerm_kubernetes_cluster.this.id
 }
 
 output "cluster_name" {
-  description = "Nazwa utworzonego klastra AKS"
+  description = "The name of the created AKS cluster"
   value       = azurerm_kubernetes_cluster.this.name
 }
 
 output "kube_config_raw" {
-  description = "Surowa konfiguracja kubeconfig (do użycia z kubectl)"
+  description = "Raw kubeconfig configuration (for use with kubectl)"
   value       = azurerm_kubernetes_cluster.this.kube_config_raw
   sensitive   = true
 }
 
 output "cluster_endpoint" {
-  description = "Adres endpointu API serwera Kubernetes"
+  description = "The API server endpoint address for Kubernetes"
   value       = azurerm_kubernetes_cluster.this.kube_config[0].host
 }
 
 output "identity_principal_id" {
-  description = "Principal ID tożsamości zarządzanej klastra (do nadawania uprawnień)"
+  description = "The Principal ID of the cluster's managed identity (for assigning permissions)"
   value       = azurerm_kubernetes_cluster.this.identity[0].principal_id
 }
--- variables.tf
+++ variables.tf
@@ -1,43 +1,43 @@
 variable "resource_group_name" {
-  description = "Nazwa grupy zasobów, w której powstanie AKSs"
+  description = "Name of the resource group where the AKS will be created"
   type        = string
 }
 
 variable "location" {
-  description = "Lokalizacja (region) Azure"
+  description = "Azure location (region)"
   type        = string
 }
 
 variable "cluster_name" {
-  description = "Nazwa klastra AKS"
+  description = "Name of the AKS cluster"
   type        = string
 }
 
 variable "dns_prefix" {
-  description = "Prefix DNS dla klastra"
+  description = "DNS prefix for the cluster"
   type        = string
 }
 
 variable "kubernetes_version" {
-  description = "Wersja Kubernetes"
+  description = "Kubernetes version"
   type        = string
-  default     = "1.29" # Możesz zmienić na nowszą lub użyć null dla domyślnej
+  default     = "1.29" # You can change to a newer version or use null for the default
 }
 
 variable "node_count" {
-  description = "Początkowa liczba węzłów (nodes)"
+  description = "Initial number of nodes"
   type        = number
   default     = 2
 }
 
 variable "vm_size" {
-  description = "Rozmiar maszyny wirtualnej dla węzłów"
+  description = "Virtual machine size for the nodes"
   type        = string
   default     = "Standard_D2_v2"
 }
 
 variable "tags" {
-  description = "Tagi do przypisania do zasobów"
+  description = "Tags to assign to resources"
   type        = map(string)
   default     = {}
 }
